e1a66d9f12d705232ebe52448ee6f165dad81850
Jalinson Diaz <zjaaaldev@gmail.com>
2022-07-05 20:57:11 -0300

    Fixed some misplaced components

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,19 +1,24 @@
-import { Grid, Typography, useTheme } from "@mui/material";
-import NotePage from "../pages/home/NotePage";
+import { Grid, useTheme } from "@mui/material";
+import { useSelector } from "react-redux";
 import NothingSelected from "./NothingSelected";
+import Note from "./Note";
+
 const MainContent = () => {
+  const { active } = useSelector((state) => state.notes);
   const theme = useTheme();
   return (
     <Grid
       container
       item
       xs={8}
+      direction={"column"}
+      justifyContent={"center"}
+      alignItems={"center"}
       sx={{
         backgroundColor: `${theme.palette.grey["900"]}`,
       }}
     >
-      {/* <NothingSelected /> */}
-      <NotePage />
+      {active ? <Note /> : <NothingSelected />}
     </Grid>
   );
 };
--- src/pages/home/NotePage.jsx
+++ src/pages/home/NotePage.jsx
@@ -1,21 +1,9 @@
-import React from "react";
-import Grid from "@mui/material/Grid";
-import NotesAppBar from "../../components/NotesAppBar";
-import { useSelector } from "react-redux";
-import NothingSelected from "../../components/NothingSelected";
-import Note from "../../components/Note";
+import { Grid, useTheme } from "@mui/material";
+import MainContent from "../../components/MainContent";
 const NotePage = () => {
-  const { active } = useSelector((state) => state.notes);
-
   return (
-    <Grid
-      container
-      item
-      direction={"column"}
-      justifyContent={"center"}
-      alignItems={"center"}
-    >
-      {active ? <Note /> : <NothingSelected />}
+    <Grid container item direction={"column"}>
+      <MainContent />
     </Grid>
   );
 };

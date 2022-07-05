63c9a4ea03ff5b2e038544ecbefe5fa5a653eb24
Jalinson Diaz <zjaaaldev@gmail.com>
2022-07-05 00:16:15 -0300

    Added memorization of auth credentials and loading screen

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,16 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import { Container } from "@mui/material";
+import { CircularProgress, Grid } from "@mui/material";
+import { firebase } from "../firebase/firebase-config";
+import { useDispatch } from "react-redux";
 import AuthRouter from "./AuthRouter";
 import HomePage from "../pages/home/HomePage";
+import { login } from "../actions/auth";
 
 const AppRouter = () => {
-  return (
-    <Routes>
-      <Route path="/auth/*" element={<AuthRouter />} />
-      <Route exact path="/" element={<HomePage />} />
-    </Routes>
-  );
+  const dispatch = useDispatch();
+
+  const [checking, setChecking] = useState(true);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user?.uid) {
+        dispatch(login(user.uid, user.displayName));
+      }
+
+      setChecking(false);
+    });
+  }, [dispatch, checking]);
+
+  if (checking) {
+    return (
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        alignContent="center"
+        height={"100vh"}
+      >
+        <CircularProgress color="secondary" />
+      </Grid>
+    );
+  } else
+    return (
+      <Routes>
+        <Route path="/auth/*" element={<AuthRouter />} />
+        <Route exact path="/" element={<HomePage />} />
+      </Routes>
+    );
 };
 
 export default AppRouter;
